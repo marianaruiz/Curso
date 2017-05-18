ac88e573b04c6f633747ef1635acc85c530a41fe
marianaruiz <marian.ayo@hotmail.com>
2017-05-18 18:04:08 -0500

    uno

---
+++ src/main/java/curso/projecto2/HolaMundo.java
@@ -0,0 +1,15 @@
+package curso.projecto2;
+
+import org.omg.CORBA.ARG_IN;
+
+public class HolaMundo {
+    public static void main(String[] args) {
+        System.out.println("package curso.projecto3;");
+        System.out.println("import org.omg.CORBA.ARG_IN;");
+        System.out.println("public class HolaMundo {");
+        System.out.println("public static void main (String [ ] args) {");
+        System.out.println("System.out.println(\"Hola Mundo\");");
+        System.out.println("}");
+        System.out.println("}");
+    }
+}
